7f60adcbb26ddc3dc2eb5d2d5a408c120c76966e
neobats <batton.guy@gmail.com>
2023-05-08 09:53:42 -0400

    initial setup with theme; no mappings

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,12 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
+  -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.kanagawa" },
+  { import = "astrocommunity.media.presence-nvim" }
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
